6afc209799b5e3003dc67ced80bd232fa3a5ed27
kebabtent <kebabtent@gmail.com>
2020-12-05 17:34:02 +0100

    Day 05

---
+++ day05/src/main.rs
@@ -0,0 +1,23 @@
+use common::read_lines;
+
+fn s(x: &str) -> u16 {
+	let x = x
+		.replace("F", "0")
+		.replace("B", "1")
+		.replace("L", "0")
+		.replace("R", "1");
+	u16::from_str_radix(&x, 2).unwrap()
+}
+
+fn main() {
+	let mut ss: Vec<_> = read_lines("05").map(|l| s(&l)).collect();
+	ss.sort_unstable();
+	println!("{}", ss.last().unwrap());
+	let i = ss
+		.iter()
+		.zip(ss.iter().skip(1))
+		.find(|(&a, &b)| a + 1 != b)
+		.map(|(&a, _)| a + 1)
+		.unwrap();
+	println!("{}", i);
+}
