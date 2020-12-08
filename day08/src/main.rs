260bbd4edfd348ea5ead4de6e7ce816891f98635
kebabtent <kebabtent@gmail.com>
2020-12-08 19:03:04 +0100

    Day 08: shorten

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,6 +1,7 @@
 use common::read_lines;
 use numext_fixed_uint::U1024;
 use std::iter;
+use O::*;
 
 struct S {
 	i: usize,
@@ -20,21 +21,20 @@ impl S {
 }
 
 #[derive(Copy, Clone, PartialEq)]
-enum Op {
-	NOP(i16),
-	ACC(i16),
-	JMP(i16),
-	OK,
+enum O {
+	N(i16),
+	A(i16),
+	J(i16),
+	K,
 }
-use Op::*;
 
-impl Op {
+impl O {
 	fn p(i: &str) -> Option<Self> {
 		let j = i[4..].parse().ok()?;
 		let o = match &i[..3] {
-			"nop" => NOP(j),
-			"acc" => ACC(j),
-			"jmp" => JMP(j),
+			"nop" => N(j),
+			"acc" => A(j),
+			"jmp" => J(j),
 			_ => return None,
 		};
 		Some(o)
@@ -42,23 +42,23 @@ impl Op {
 
 	fn a(&self, s: S) -> S {
 		match *self {
-			NOP(_) => s.u(1, 0),
-			ACC(x) => s.u(1, x),
-			JMP(x) => s.u(x, 0),
-			OK => s,
+			N(_) => s.u(1, 0),
+			A(x) => s.u(1, x),
+			J(x) => s.u(x, 0),
+			K => s,
 		}
 	}
 
 	fn f(&mut self) {
 		*self = match *self {
-			NOP(x) => JMP(x),
-			JMP(x) => NOP(x),
+			N(x) => J(x),
+			J(x) => N(x),
 			x => x,
 		};
 	}
 }
 
-fn r(o: &Vec<Op>) -> S {
+fn r(o: &Vec<O>) -> S {
 	let mut s = S::n();
 	let mut b = U1024::zero();
 	loop {
@@ -73,8 +73,8 @@ fn r(o: &Vec<Op>) -> S {
 
 fn main() {
 	let mut o = read_lines("08")
-		.filter_map(|l| Op::p(&l))
-		.chain(iter::once(OK))
+		.filter_map(|l| O::p(&l))
+		.chain(iter::once(K))
 		.collect::<Vec<_>>();
 
 	let a = r(&o).a;
@@ -84,7 +84,7 @@ fn main() {
 	loop {
 		o[t].f();
 		let s = r(&o);
-		if o[s.i] == OK {
+		if o[s.i] == K {
 			b = s.a;
 			break;
 		}
