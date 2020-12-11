b537aabda93afafe01f0fb964b3f68411b229407
kebabtent <kebabtent@gmail.com>
2020-12-11 20:44:06 +0100

    Day 10: simplify

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -6,22 +6,19 @@ fn main() {
 		.chain(read_lines("10").filter_map(|l| l.parse::<u8>().ok()))
 		.collect::<Vec<_>>();
 	r.sort_unstable();
-	r.push(r.last().copied().unwrap() + 3);
+	for i in 0..(r.len() - 1) {
+		r[i] = r[i + 1] - r[i];
+	}
+	*r.last_mut().unwrap() = 3;
 
-	let a = r
-		.iter()
-		.zip(r.iter().skip(1))
-		.map(|(a, b)| b - a - 1)
-		.fold(0u32, |a, x| a + (1 << 8 * x));
+	let a = r.iter().map(|v| v - 1).fold(0u32, |a, x| a + (1 << 8 * x));
 	let a = (a >> 16) * (a & 0xFF);
 	let b = r
 		.iter()
-		.zip(r.iter().skip(1))
-		.map(|(a, b)| b - a)
 		.peekable()
 		.batching(|it| {
 			it.peek()?;
-			let n = it.take_while(|&v| v < 3).count().saturating_sub(1);
+			let n = it.take_while(|&&v| v < 3).count().saturating_sub(1);
 			Some(n * (n + 1) / 2 + 1)
 		})
 		.product::<usize>();
